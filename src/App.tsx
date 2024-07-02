21c7b38b6f2657bbd1c8f04c7b671c3a8107065f
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-07-02 11:57:44 +0100

    using CountStore in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useCounterStore } from "./store";
 
-function App() {
+const App = () => {
   const { count, increaseCount } = useCounterStore();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Parent component</h1>
       <div className="card">
         <button onClick={() => increaseCount()}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <OtherComponent />
     </>
   );
-}
+};
 
+const OtherComponent = () => {
+  const { count, resetCount } = useCounterStore();
+  return (
+    <div className="component">
+      <p>Child component</p>
+      <h1>Count : {count}</h1>
+      <button onClick={() => resetCount()}>reset count</button>
+    </div>
+  );
+};
 export default App;
--- src/store.ts
+++ src/store.ts
@@ -3,9 +3,11 @@ import { create } from "zustand";
 type CounterStore = {
   count: number;
   increaseCount: () => void;
+  resetCount: () => void;
 };
 
 export const useCounterStore = create<CounterStore>((set) => ({
   count: 0,
   increaseCount: () => set((state) => ({ count: state.count + 1 })),
+  resetCount: () => set(() => ({ count: 0 })),
 }));
